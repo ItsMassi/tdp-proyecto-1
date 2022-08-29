253c0d3198d34d826c76e9390f30439f99f5b199
ItsMassi <90329965+ItsMassi@users.noreply.github.com>
2022-08-29 00:36:28 -0300

    Add: complements and final touches
    agrego el codigo para el nombre del usuario, los nombres y las fotos.

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -3,12 +3,14 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -18,28 +20,81 @@ public class SimplePresentationScreen extends JFrame {
 	private JTabbedPane tabbedPane;
 	private Student studentData;
 
-	public SimplePresentationScreen(Student studentData) {
+
+	public SimplePresentationScreen( Student studentData ) {
+		setResizable(false);
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentación");
+		setIconImage(new ImageIcon("C:\\Users\\massi\\Documents\\GitHub\\tdp-proyecto-1\\src\\images\\tdp.png").getImage());
+		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentaciÃ³n");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(new Dimension(615, 250));
-		setResizable(false);
 		setContentPane(contentPane);
 		
 		init();
 	}
 	
 	private void init() {
+		contentPane.setLayout(null);
 		// Tabbed Pane to student personal data
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(5, 5, 430, 201);
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
-		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
-		contentPane.add(tabbedPane, BorderLayout.WEST);
+		tabbedPane.addTab("InformaciÃ³n del alumno", null, tabInformation, "Muestra la informaciÃ³n declarada por el alumno");
+		tabInformation.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("LU:");
+		lblNewLabel.setBounds(10, 22, 46, 14);
+		tabInformation.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Apellido: ");
+		lblNewLabel_1.setBounds(10, 47, 46, 14);
+		tabInformation.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Nombre:");
+		lblNewLabel_2.setBounds(10, 72, 46, 14);
+		tabInformation.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Email:");
+		lblNewLabel_3.setBounds(10, 97, 46, 14);
+		tabInformation.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Github URL:");
+		lblNewLabel_4.setBounds(10, 122, 70, 14);
+		tabInformation.add(lblNewLabel_4);
+		
+		JTextPane studentID = new JTextPane();
+		studentID .setText(String.valueOf(studentData.getId()));
+		studentID .setBounds(90, 16, 325, 20);
+		tabInformation.add(studentID);
+		
+		JTextPane studentLastname = new JTextPane();
+		studentLastname.setText(studentData.getLastName());
+		studentLastname.setBounds(90, 41, 325, 20);
+		tabInformation.add(studentLastname);
+		
+		JTextPane studentName = new JTextPane();
+		studentName.setText(studentData.getFirstName());
+		studentName.setBounds(90, 66, 325, 20);
+		tabInformation.add(studentName);
+		
+		JTextPane emailinfo = new JTextPane();
+		emailinfo.setText(studentData.getMail());
+		emailinfo.setBounds(90, 91, 325, 20);
+		tabInformation.add(emailinfo);
+		
+		JTextPane github = new JTextPane();
+		github.setText(studentData.getGithubURL());
+		github.setBounds(90, 116, 325, 20);
+		tabInformation.add(github);
+		contentPane.add(tabbedPane);
+		
+		JLabel lblNewLabel_5 = new JLabel(new ImageIcon(studentData.getPathPhoto()));
+		lblNewLabel_5.setBounds(445, 26, 144, 174);
+		contentPane.add(lblNewLabel_5);
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,16 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+                SimplePresentationScreen frame = new SimplePresentationScreen(new Student(132735, "Massiris", "Daniel", "danielmassiris10@gmail.com", "https://github.com/ItsMassi", "C:\\Users\\massi\\Documents\\GitHub\\tdp-proyecto-1\\src\\images\\DanielM.jpg"));
+                frame.setVisible(true);
             }
         });
     }
